528813e04f78d47714f1872b362e14a470b4c987
Stepan Anokhin <stepan.anokhin@gmail.com>
2021-06-08 19:31:26 +0700

    Improve windows test runner

--- sources/scripts/run_tests.bat
+++ sources/scripts/run_tests.bat
@@ -1,14 +1,15 @@
+@echo off
 
-@set m20=%1
-@set test_dir=%2
-@set timestamp=%DATE:/=-%_%TIME::=-%
-@set timestamp=%timestamp: =%
-@set run_dir=%test_dir%\run-%timestamp%-%RANDOM%
+set m20=%1
+set test_dir=%2
+set timestamp=%DATE:/=-%_%TIME::=-%
+set timestamp=%timestamp: =%
+set run_dir=%test_dir%\run-%timestamp%-%RANDOM%
 
-@echo RUN TESTS
-@echo
-@echo M-20 Emulator:     %M20%
-@echo Tests Directory:   %test_dir%
-@echo Run Directory:     %run_dir%
-@echo
+echo RUN TESTS
+echo.
+echo M-20 Emulator:     %M20%
+echo Tests Directory:   %test_dir%
+echo Run Directory:     %run_dir%
+echo.
 
