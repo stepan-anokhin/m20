0280e4dbd0a889b3249ae76cf98bceba0de893f9
Stepan Anokhin <stepan.anokhin@gmail.com>
2021-06-08 19:48:46 +0700

    Improve windows test runner

--- sources/scripts/run_tests.bat
+++ sources/scripts/run_tests.bat
@@ -19,5 +19,5 @@ for %I in (*.simh *.cdr *.m20 *.drum0) do copy %I %run_dir%
 cd %run_dir%
 
 set test_count=0
-for %%A in (*.simh) do set /a test_count+=1
+for %I in (*.simh) do set /a test_count=test_count+1
 echo Running %test_count% tests
